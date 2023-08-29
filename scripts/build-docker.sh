09762254a87c0b43c30d50c61bbd6929b07a33ad
cospan <cospan@gmail.com>
2023-08-29 08:56:06 -0400

    Moved the reference 'axi' directory to not confuse users

--- scripts/build-docker.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-time docker image build                 \
-    --file install_swbuilder.dockerfile \
-    --tag cocotb-builder .
-
-docker image ls
--- scripts/new-ip-core.sh
+++ scripts/new-ip-core.sh
@@ -7,10 +7,8 @@ set -e
 SCRIPT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 TYPE="axils"
 NAME="NONAME"
-IP_PATH="../axi/axi_lite_slave"
-DEST_PATH="$SCRIPT_PATH/../cores"
-#DEST_PATH="/home/cospan/sandbox"
-SOURCE_PATH="$SCRIPT_PATH/$IP_PATH"
+IP_PATH="axi/axi_lite_slave"
+DEST_PATH="${SCRIPT_PATH}/../cores"
 
 
 
@@ -35,9 +33,9 @@ do
 done
 
 # Is this is a valid type?
-if [ $TYPE != "axils" ]
+if [ ${TYPE} != "axils" ]
 	then
-		echo "Unrecognized type: $TYPE";
+		echo "Unrecognized type: ${TYPE}";
 		echo "Valid types:"
 		echo "	axils (AXI Lite Slave)";
 		exit
@@ -45,17 +43,16 @@ fi
 
 
 # Convert type to paths
-if [ $TYPE = "axils" ]
+if [ ${TYPE} = "axils" ]
 	then
-		IP_PATH="../axi/axi_lite_slave"
+		IP_PATH="axi/axi_lite_slave"
 fi
-
-SOURCE_PATH="$SCRIPT_PATH/$IP_PATH"
+SOURCE_PATH="${SCRIPT_PATH}/${IP_PATH}"
 
 #Convert all relative elements to absolute
-SOURCE_PATH=`cd "$SOURCE_PATH"; pwd`
+SOURCE_PATH=`cd "${SOURCE_PATH}"; pwd`
 
-# Debug Stuff
+## Debug Stuff
 #echo "Name: $NAME";
 #echo "Source Path: $SOURCE_PATH";
 #echo "Destination Path: $DEST_PATH";
@@ -63,22 +60,22 @@ SOURCE_PATH=`cd "$SOURCE_PATH"; pwd`
 #echo "Script Path: $SCRIPT_PATH";
 #echo "IP Path: $IP_PATH";
 
-MODIFY_PATH="$DEST_PATH/$NAME"
+MODIFY_PATH="${DEST_PATH}/${NAME}"
 #echo "MODIFY Path: $MODIFY_PATH"
 
-echo "Creating Directory: $MODIFY_PATH (if it doesn't already exist)"
-mkdir -p $MODIFY_PATH
+echo "Creating Directory: ${MODIFY_PATH} (if it doesn't already exist)"
+mkdir -p ${MODIFY_PATH}
 
 # Move all the files to the new location
-echo "Copy the template project from $SOURCE_PATH to $MODIFY_PATH"
-cp -a $SOURCE_PATH/* $MODIFY_PATH
+echo "Copy the template project from ${SOURCE_PATH} to ${MODIFY_PATH}"
+cp -a ${SOURCE_PATH}/* ${MODIFY_PATH}
 
-echo "Rename everything in files to the user provided name: $NAME"
-grep -riInl 'NAME' $MODIFY_PATH | xargs sed -i "s/NAME/$NAME/g"
+echo "Rename everything in files to the user provided name: ${NAME}"
+grep -riInl 'NAME' ${MODIFY_PATH} | xargs sed -i "s/NAME/${NAME}/g"
 
-echo "Rename the main core file to the user provided name: $NAME.v"
-mv $MODIFY_PATH/hdl/NAME.v $MODIFY_PATH/hdl/$NAME.v
-mv $MODIFY_PATH/tests/NAME_driver.py $MODIFY_PATH/tests/${NAME}_driver.py
+echo "Rename the main core file to the user provided name: ${NAME}.v"
+mv ${MODIFY_PATH}/hdl/NAME.v ${MODIFY_PATH}/hdl/${NAME}.v
+mv ${MODIFY_PATH}/tests/NAME_driver.py ${MODIFY_PATH}/tests/${NAME}_driver.py
 
 
 
